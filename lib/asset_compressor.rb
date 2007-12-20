ea5129864e166a3a48faf212dec030fef58ef10a
Paul Mucur <mudge@mudge.name>
2007-12-20 20:31:10 +0000

    Concatenates files with Ruby and removed variables_for_type architecture.
    In an attempt to increase cross-platform compatibility, I have removed the
    dependency on the `cat` command-line utility in favour of Ruby's IO capabilities.
    Gone are the variables_for_javascript and variables_for_stylesheet in favour of
    more detailed javascript_include_compressed and stylesheet_include_compressed
    methods which, in turn, use include_compressed's improved interface.
    
    `include_compressed` now takes an asset type's tag helper as a Proc object as well
    as the compressed file name and sources which are pre-formatted to be relative
    from RAILS_ROOT/public.
    
    A minor cosmetic change was to switch from using the Proc array notation to
    the slightly less confusing `call` method.

--- lib/asset_compressor.rb
+++ lib/asset_compressor.rb
@@ -11,11 +11,13 @@ module AssetCompressor
   # Both javascript_include_compressed and stylesheet_link_compressed do very similar
   # things so they simply call a private third method, include_compressed.
   def javascript_include_compressed(compressed_file_name, *javascripts)
-    include_compressed(:javascript, compressed_file_name, *javascripts)
+    javascripts.map! { |javascript| path_without_asset_id(javascript_path(javascript)) }
+    include_compressed(Proc.new { |sources| javascript_include_tag(*sources) }, path_without_asset_id(javascript_path(compressed_file_name)), *javascripts)
   end
   
   def stylesheet_link_compressed(compressed_file_name, *stylesheets)
-    include_compressed(:stylesheet, compressed_file_name, *stylesheets)
+    stylesheets.map! { |stylesheet| path_without_asset_id(stylesheet_path(stylesheet)) }
+    include_compressed(Proc.new { |sources| stylesheet_link_tag(*sources) }, path_without_asset_id(stylesheet_path(compressed_file_name)), *stylesheets)
   end
   
   private
@@ -25,54 +27,53 @@ module AssetCompressor
     RAILS_ENV == environment
   end
   
-  # Given a string like "jquery" or "jquery.js", return the full filename of "jquery.js".
-  def javascript_path(path)
-    !path[/\.js$/] ? "#{path}.js" : path
-  end
-  
-  # Given a string like "scaffold" or "scaffold.css", return the full filename of 
-  # "scaffold.css".
-  def stylesheet_path(path)
-    !path[/\.css$/] ? "#{path}.css" : path
-  end
-  
-  def variables_for_javascript(compressed_file_name, scripts)
-    extension = ".js"
-    folder = "javascripts"
-    compressed_file_name = javascript_path(compressed_file_name)
-    scripts = scripts.map { |script| javascript_path(script) }
-    include_tag = Proc.new { |script| javascript_include_tag(script) }
-    [extension, folder, compressed_file_name, scripts, include_tag]
-  end
-  
-  def variables_for_stylesheet(compressed_file_name, scripts)
-    extension = ".css"
-    folder = "stylesheets"
-    compressed_file_name = stylesheet_path(compressed_file_name)
-    scripts = scripts.map { |script| stylesheet_path(script) }
-    include_tag = Proc.new { |script| stylesheet_link_tag(script) }
-    [extension, folder, compressed_file_name, scripts, include_tag]
+  # While Rails' javascript_path and stylesheet_path will nicely deal with filenames for us
+  # it also appends an asset ID query string to paths (e.g. "base.css?123218234") which is
+  # useful for client-side caching but renders the paths useless for normal IO (as they
+  # are taken as part of the file name). This simple regular expression strips these
+  # query strings.
+  def path_without_asset_id(asset_path)
+    asset_path[/^[^?]+/]
   end
   
   # The main method that handles both the compression of files and the returning of the
   # appropriate HTML tag.
-  def include_compressed(type, compressed_file_name, *scripts)
-    extension, folder, compressed_file_name, scripts, include_tag = send("variables_for_#{type}", compressed_file_name, scripts)
-    
+  #
+  # It takes three mandatory arguments:
+  # 
+  # => tag_helper: a Proc object for the tag helper method to use for this asset type.
+  # => compressed_file_name: the path to the asset relative from RAILS_ROOT/public
+  # => *sources: one or more actual sources to be compressed.
+  def include_compressed(tag_helper, compressed_file_name, *sources)
+
+    # If we are to compress the files given (e.g. when in the correct environment) then do so.
     if compress?
-      if File.exists?(File.join(RAILS_ROOT, 'public', folder ,compressed_file_name))
-        include_tag[compressed_file_name]
-      else
-        script_paths = scripts.map { |path| File.join(RAILS_ROOT, 'public', folder, path) }
-  
-        # Concatenate the files.
-        `cat #{script_paths.join(" ")} > #{RAILS_ROOT}/tmp/#{compressed_file_name}`
-        `java -jar #{File.join(RAILS_ROOT, 'vendor', 'plugins', 'asset_compressor', 'lib', 'yuicompressor-2.2.4.jar')} #{File.join(RAILS_ROOT, 'tmp', compressed_file_name)} -o #{File.join(RAILS_ROOT, 'public', folder, compressed_file_name)}`
-        File.delete(File.join(RAILS_ROOT, 'tmp', compressed_file_name)) if File.exists?(File.join(RAILS_ROOT, 'tmp', compressed_file_name))
-        include_tag[compressed_file_name]
+      
+      # If the compressed file does not already exist, create it.
+      if !File.exists?(File.join(RAILS_ROOT, 'public', compressed_file_name))
+        
+        # Concatenate the files together in Ruby as Windows does not have cat.
+        File.open(File.join(RAILS_ROOT, 'tmp', File.basename(compressed_file_name)), 'w') do |concatenated_file|
+          sources.each do |source|
+            File.open(File.join(RAILS_ROOT, 'public', source), 'r') do |file|
+              concatenated_file << file.read
+            end
+          end
+        end
+        
+        # Compress the concatenated file.
+        `java -jar #{File.join(RAILS_ROOT, 'vendor', 'plugins', 'asset_compressor', 'lib', 'yuicompressor-2.2.4.jar')} #{File.join(RAILS_ROOT, 'tmp', File.basename(compressed_file_name))} -o #{File.join(RAILS_ROOT, 'public', compressed_file_name)}`
+      
+        # Delete the concatenated file.
+        File.delete(File.join(RAILS_ROOT, 'tmp', File.basename(compressed_file_name))) if File.exists?(File.join(RAILS_ROOT, 'tmp', File.basename(compressed_file_name)))
       end
+      
+      # Include tag for compressed file.
+      tag_helper.call(compressed_file_name)
     else
-      scripts.map { |script| include_tag[script] }.join
+      
+      # Include tags for each file separately.
+      tag_helper.call(*sources)
     end
   end
 end
