8a588174b621ea26e991fa1ac37a44f029110ad7
Paul Mucur <mudge@mudge.name>
2008-06-25 10:59:23 +0100

    Now supports compression in multiple environments as well as some tidying up and minor performance enhancements.

--- lib/asset_compressor.rb
+++ lib/asset_compressor.rb
@@ -1,17 +1,20 @@
 module AssetCompressor
   
-  YUI_COMPRESSOR_VERSION = "2.3.5"
+  # If you wish to use your own version of the YUI Compressor, simply
+  # override this constant to its path.
+  YUI_COMPRESSOR = "#{File.dirname(__FILE__)}/yuicompressor-2.3.5.jar"
   
-  # By default, Javascripts and stylesheets will only be compressed when in production.
-  # However, this can be overridden by changing the value of AssetCompressor.environment
-  # in one of your initializers.
+  # By default, assets will only be compressed when in production.
+  # However, this can be overridden by changing the value of 
+  # AssetCompressor.environments in one of your initializers.
   #
-  # e.g. AssetCompressor.environment = "development" will enable compression in development.
-  @@environment = "production"
-  mattr_accessor :environment
-  
-  # Both javascript_include_compressed and stylesheet_link_compressed do very similar
-  # things so they simply call a private third method, include_compressed.
+  # e.g. AssetCompressor.environments << "development" 
+  mattr_accessor :environments
+  self.environments = %w{production}
+
+  # Both javascript_include_compressed and stylesheet_link_compressed do very 
+  # similar things so they simply call a private third method, 
+  # include_compressed.
   def javascript_include_compressed(compressed_file_name, *javascripts)
     javascripts.map! { |javascript| path_without_asset_id(javascript_path(javascript)) }
     include_compressed(path_without_asset_id(javascript_path(compressed_file_name)), *javascripts) { |sources| javascript_include_tag(*sources) }
@@ -26,36 +29,41 @@ module AssetCompressor
   
   # Returns a boolean whether to compress files or not.
   def compress?
-    RAILS_ENV == environment
+    environments.include?(RAILS_ENV)
   end
   
-  # While Rails' javascript_path and stylesheet_path will nicely deal with filenames for us
-  # it also appends an asset ID query string to paths (e.g. "base.css?123218234") which is
-  # useful for client-side caching but renders the paths useless for normal IO (as they
-  # are taken as part of the file name). This simple regular expression strips these
-  # query strings.
+  # While Rails' javascript_path and stylesheet_path will nicely deal with 
+  # filenames for us it also appends an asset ID query string to paths 
+  # (e.g. "base.css?123218234") which is useful for client-side caching but 
+  # renders the paths useless for normal IO (as they are taken as part of the 
+  # file name). This simple regular expression strips these query strings.
   def path_without_asset_id(asset_path)
     asset_path[/^[^?]+/]
   end
   
-  # The main method that handles both the compression of files and the returning of the
-  # appropriate HTML tag.
+  # The main method that handles both the compression of files and the 
+  # returning of the appropriate HTML tag.
   #
-  # It takes the following two mandatory arguments and a block containing the tag helper for 
-  # the asset type:
+  # It takes the following two mandatory arguments and a block containing the 
+  # tag helper for the asset type:
   # 
-  # => compressed_file_name: the path to the asset relative from RAILS_ROOT/public
+  # => compressed_file_name: the path to the asset relative from 
+  #                          RAILS_ROOT/public
   # => *sources: one or more actual sources to be compressed.
   def include_compressed(compressed_file_name, *sources)
 
-    # If we are to compress the files given (e.g. when in the correct environment) then do so.
+    # If we are to compress the files given (e.g. when in the correct 
+    # environment) then do so.
     if compress?
       
+      tmp_filename = File.join(RAILS_ROOT, 'tmp', File.basename(compressed_file_name))
+      compressed_filename = File.join(RAILS_ROOT, 'public', compressed_file_name)
+      
       # If the compressed file does not already exist, create it.
-      if !File.exists?(File.join(RAILS_ROOT, 'public', compressed_file_name))
+      if !File.exists?(compressed_filename)
         
         # Concatenate the files together in Ruby as Windows does not have cat.
-        File.open(File.join(RAILS_ROOT, 'tmp', File.basename(compressed_file_name)), 'w') do |concatenated_file|
+        File.open(tmp_filename, 'w') do |concatenated_file|
           sources.each do |source|
             File.open(File.join(RAILS_ROOT, 'public', source), 'r') do |file|
               concatenated_file << file.read
@@ -64,7 +72,7 @@ module AssetCompressor
         end
         
         # Compress the concatenated file with YUI Compressor.
-        `java -jar #{File.join(RAILS_ROOT, 'vendor', 'plugins', 'asset_compressor', 'lib', "yuicompressor-#{YUI_COMPRESSOR_VERSION}.jar")} #{File.join(RAILS_ROOT, 'tmp', File.basename(compressed_file_name))} -o #{File.join(RAILS_ROOT, 'public', compressed_file_name)}`
+        `java -jar #{YUI_COMPRESSOR} #{tmp_filename} -o #{compressed_filename}`
       
         # Delete the concatenated file.
         File.delete(File.join(RAILS_ROOT, 'tmp', File.basename(compressed_file_name))) if File.exists?(File.join(RAILS_ROOT, 'tmp', File.basename(compressed_file_name)))
